38fc429e25743433d0168473016bc62ff0086287
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2018-11-17 11:18:11 +0000

    added main api set-up

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 /*
  * This assigbment no 1 fo the NODE Masterclass
  * Create simmple API route /hello with welcome message
+ * 
+ * 
+ * 
  * author: Byron Dunkley
  * 
  * 
  * 
  */
+
+ const server = require('./lib/server');
+
+
+// Declare app
+ const app = {}
+
+
+ const app.init = () => {
+     // start server
+     server.init();
+ }
+
+// Start app
+app.init()
+
+// Export app
+module.exports = app;
